3ffc5650e1aaeed62ee8004f101195f3f0d21f6c
Illia Filippov <filippov.illia.ukr@gmail.com>
2022-03-22 17:30:27 +0100

    Variables

--- 1_variables.js
+++ 1_variables.js
@@ -26,3 +26,16 @@ console.log("push 5 to arr");
 arr.push(5); // Correct
 
 console.log("const arr = " + arr);
+
+/**Initialize Variables
+ * It is a good coding practice to initialize variables when you declare them.
+ * Avoid undefined values.
+ * Don't Use new Object().
+ */
+ let str1 = "";             // new primitive string
+ let num2 = 0;              // new primitive number
+ let bool3 = false;         // new primitive boolean
+ const obj4 = {};           // new object
+ const arr5 = [];           // new array object
+ const reg6 = /()/;         // new regexp object
+ const fun7 = function(){}; // new function object
